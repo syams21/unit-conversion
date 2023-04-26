9cd88f81e6acc67fdca717210b9f30b77347204f
Syams <syams21arifin@gmail.com>
2023-04-26 09:51:03 +0700

    unit conversion

--- unitConversion.ts
+++ unitConversion.ts
@@ -1,39 +1,58 @@
 enum Unit
 {
-    Meter = "meter",
-    Kilometer = "kilometer",
-    Mile = "mile",
-  }
+  Meter = "meter",
+  Kilometer = "kilometer",
+  Mile = "mile",
+}
   
-  function convertLength(value: number, from: Unit, to: Unit): number {
+  function convertLength(value: number, from: Unit, to: Unit): number
+  {
     let result: number = 0;
   
     // convert from meter to destination unit
-    switch (from) {
+    switch (from)
+    {
       case Unit.Meter:
-        if (to === Unit.Kilometer) {
+        if (to === Unit.Kilometer)
+        {
           result = value / 1000;
-        } else if (to === Unit.Mile) {
+        }
+        else if (to === Unit.Mile)
+        {
           result = value * 0.000621371;
-        } else {
+        }
+        else
+        {
           result = value;
         }
         break;
+
       case Unit.Kilometer:
-        if (to === Unit.Meter) {
+        if (to === Unit.Meter)
+        {
           result = value * 1000;
-        } else if (to === Unit.Mile) {
+        }
+        else if (to === Unit.Mile)
+        {
           result = value * 0.621371;
-        } else {
+        }
+        else
+        {
           result = value;
         }
         break;
+
       case Unit.Mile:
-        if (to === Unit.Meter) {
+        if (to === Unit.Meter)
+        {
           result = value * 1609.34;
-        } else if (to === Unit.Kilometer) {
+        }
+        else if (to === Unit.Kilometer)
+        {
           result = value * 1.60934;
-        } else {
+        }
+        else
+        {
           result = value;
         }
         break;
